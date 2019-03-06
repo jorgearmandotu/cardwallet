5f9a617c157b084f369298a16438041833cfa783
armando <jorgearmandou@gmail.com>
2019-03-05 21:44:08 -0500

    probando

--- lib/bloc/card_bloc.dart
+++ lib/bloc/card_bloc.dart
@@ -9,7 +9,7 @@ class CardBloc {
   BehaviorSubject<String> _cardYear = BehaviorSubject<String>();
   BehaviorSubject<String> _cardCvv = BehaviorSubject<String>();
   BehaviorSubject<String> _cardType =BehaviorSubject<String>();
-  BehaviorSubject<int> _cardColorIndexSelected = BehaviorSubject<int>(seedValue: 0);
+  //BehaviorSubject<int> _cardColorIndexSelected = BehaviorSubject<int>(seedValue: 0);
 
   final _cardsColors =BehaviorSubject<List<CardColorModel>>();
 
@@ -22,13 +22,13 @@ class CardBloc {
   Function(String) get selectCardType => _cardType.sink.add;
 
   //retrive data from stream
-  Stream<String> get cardHolderName => _cardHolderName.stream.transform(validateCardHolderName);
+  /*Stream<String> get cardHolderName => _cardHolderName.stream.transform(validateCardHolderName);
   Stream<String> get cardNUmber => _cardNumber.stream.transform(validateCardNumber);
   Stream<String> get cardMonth => _cardMonth.stream.transform(validateCardMonth);
   Stream<String> get cardYear => _cardYear.stream.transform(validateCardYear);
-  Stream<String> get cardCvv => _cardCvv.stream.transform(validateCardCvv);
+  Stream<String> get cardCvv => _cardCvv.stream.transform(validateCardCvv);*/
   Stream<String> get cardType => _cardType.stream;
-  Stream<int> get cardColorIndexSelected => _cardColorIndexSelected.stream;
+  //Stream<int> get cardColorIndexSelected => _cardColorIndexSelected.stream;
 
   void dispose(){
     _cardHolderName.close();
@@ -37,7 +37,7 @@ class CardBloc {
     _cardYear.close();
     _cardCvv.close();
     _cardsColors.close();
-    _cardColorIndexSelected.close();
+    //_cardColorIndexSelected.close();
     _cardType.close();
   }
 }
